922f118fc4abd4fe46cf7165d44d05169865bb73
Nalinda D Kumarasinghe <72765309+nalinda98@users.noreply.github.com>
2024-12-08 22:06:14 +0530

    aa

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const port = process.env.PORT || 3001;
 const path = require("path");
-const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser");
 // const {
 //   fetchDatesEnable,
 //   dateEnable,
@@ -33,7 +33,7 @@ const bodyParser = require("body-parser");
 
 app.use(express.json());
 app.use(cors());
-app.use(bodyParser.json(), bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json(), bodyParser.urlencoded({ extended: true }));
 
 mongoose
   .connect(process.env.MONGO_URI, {
