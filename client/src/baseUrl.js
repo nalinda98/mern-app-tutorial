785c68675f2a8dc78dd8a4f0ddbeb2d4f1bcf564
Nalinda D Kumarasinghe <72765309+nalinda98@users.noreply.github.com>
2024-12-08 15:57:42 +0530

    a

--- client/src/baseUrl.js
+++ client/src/baseUrl.js
@@ -1,3 +1,3 @@
-const baseUrl = "https://sagfjh-gdgxapgtafdxddbj.canadacentral-01.azurewebsites.net/";
+const baseUrl = "https://sagfjh-gdgxapgtafdxddbj.canadacentral-01.azurewebsites.net";
 
 export default baseUrl;
