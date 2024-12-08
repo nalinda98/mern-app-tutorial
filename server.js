6eb3f5567a8b5529cb57d13cb88d84aa871ccd5d
Nalinda D Kumarasinghe <72765309+nalinda98@users.noreply.github.com>
2024-12-08 22:59:45 +0530

    added

--- server.js
+++ server.js
@@ -3,76 +3,43 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const port = process.env.PORT || 3001;
-const path = require("path");
-const bodyParser = require("body-parser");
-// const {
-//   fetchDatesEnable,
-//   dateEnable,
-// } = require("./controllers/dateController.js");
-// const { authenticateToken } = require("./middleware/authenticateToken.js");
-// const { isSuperAdmin, isAdmin } = require("./middleware/isAdmin.js");
-// const {
-//   addBooking,
-//   fetchBookings,
-//   fetchBookingDetails,
-//   updateBookingStatus,
-// } = require("./controllers/bookingController.js");
-// const {
-//   getUsers,
-//   getUserById,
-//   register,
-//   registerG,
-//   login,
-//   logout,
-//   token,
-//   verifyUser,
-//   getUsersDetails,
-//   updateMembersDetails,
-// } = require("./controllers/userController.js");
-// const app = express();
+
+const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(bodyParser.json(), bodyParser.urlencoded({ extended: true }));
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("Database is connected..."))
-  .catch((err) => console.log(err));
+  .catch((err) => console.log("DB NOT WORKINGGGGGGG"));
 
-// app.get("/dateEnable", fetchDatesEnable);
-// app.post("/dateEnable", authenticateToken, isSuperAdmin, dateEnable);
-// app.post("/booking", authenticateToken, isAdmin, addBooking);
-// app.get("/booking", fetchBookings);
-// app.get("/booking/details", authenticateToken, isAdmin, fetchBookingDetails);
-// app.post(
-//   "/booking/updateStatus",
-//   authenticateToken,
-//   isAdmin,
-//   updateBookingStatus
-// );
-// app.get("/user/getUsers", getUsers);
-// app.get("/user/getUser/:id", getUserById);
-// app.post("/user/register", register);
-// app.post("/user/googleRegister", registerG);
-// app.post("/user/login", login);
-// app.get("/user/logout", authenticateToken, logout);
-// app.post("/user/refresh", token);
-// app.post("/user/verify", verifyUser);
-// app.get("/user/getUsersDetails", getUsersDetails);
-// app.put(
-//   "/user/updateMemberDetails",
-//   authenticateToken,
-//   isAdmin,
-//   updateMembersDetails
-// );
+//db schema
+const userSchema = mongoose.Schema({
+  name: String,
+  lastName: String,
+});
 
-app.use(express.static("./frontend/build"));
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+//db model
+const User = new mongoose.model("User", userSchema);
+
+app.get("/get-users", (req, res) => {
+  User.find()
+    .then((users) => res.json(users))
+    .catch((err) => console.log(err));
+});
+
+app.post("/create", (req, res) => {
+  //save to mongodb and send response
+  const newUser = new User({
+    name: req.body.name,
+    lastName: req.body.lastName,
+  });
+
+  newUser
+    .save()
+    .then((user) => res.json(user))
+    .catch((err) => console.log(err));
 });
 
 app.listen(port, () => {
