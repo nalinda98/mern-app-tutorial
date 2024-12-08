4444d5b66f17df3e653d4029114f55ff3464ad87
Nalinda D Kumarasinghe <72765309+nalinda98@users.noreply.github.com>
2024-12-08 16:10:23 +0530

    ddd

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const port = process.env.PORT || 3001;
+const bodyParser = require("body-parser");
 const path = require("path");
 import dateEnable from "./routes/booking.route.js";
 
