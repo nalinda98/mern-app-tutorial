18036232956a1636b742ba25e2afac0b4f682779
Nalinda D Kumarasinghe <72765309+nalinda98@users.noreply.github.com>
2024-12-08 23:54:28 +0530

    vd

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const path = require("path");
+const { fetchDatesEnable } = require("./controllers/dateController");
 const port = process.env.PORT || 3001;
 
 const app = express();
@@ -10,26 +11,28 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-
+// Mongoose connection
 mongoose
   .connect(process.env.MONGO_URI || "mongodb+srv://user1:user@cluster0.mmap8.mongodb.net/testDataDev")
   .then(() => console.log("Database is connected..."))
-  .catch((err) => console.log(err));
+  .catch((err) => console.log("Database connection error: ", err));
 
-
-app.get("/dateEnable" , 
-  (req, res) => {
-    res.send("Hello World!");
+// Route for fetching date enable status
+app.get("/dateEnable", async (req, res) => {
+  try {
+    await fetchDatesEnable(req, res);
+  } catch (err) {
+    res.status(500).json({ error: "Error fetching date enable data." });
   }
-);
+});
 
-// production script
+// Static files for production
 app.use(express.static("./frontend/build"));
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
 });
 
-
+// Server setup
 app.listen(port, () => {
-  console.log(`Server is running on post ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
