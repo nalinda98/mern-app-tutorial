e70788c75d9ce325ac2c0e845d8813022aee4eb0
Nalinda D Kumarasinghe <72765309+nalinda98@users.noreply.github.com>
2024-12-08 23:30:03 +0530

    aded

--- controllers/dateController.js
+++ controllers/dateController.js
@@ -29,8 +29,9 @@ const dateEnable = async (req, res) => {
 
 const fetchDatesEnable = async (req, res) => {
   try {
-    const dates = await DateEnable.find().populate('createdBy', 'name');
-    res.status(200).json({ dates, success: true });
+    // const dates = await DateEnable.find().populate('createdBy', 'name');
+    // res.status(200).json({ dates, success: true });
+    res.send("Hello World!");
   } catch (error) {
     console.error("Error fetching dates:", error);
     res.status(500).json({ message: "error", success: false });
