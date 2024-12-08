d570db247f6e6b735c9b873c94e7e11a5bebfd41
Nalinda D Kumarasinghe <72765309+nalinda98@users.noreply.github.com>
2024-12-08 23:41:33 +0530

    a

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const path = require("path");
-const { fetchDatesEnable } = require("./controllers/dateController");
 const port = process.env.PORT || 3001;
 
 const app = express();
@@ -11,21 +10,25 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+
 mongoose
-  .connect(
-    process.env.MONGO_URI ||
-      "mongodb+srv://user1:user@cluster0.mmap8.mongodb.net/testDataDev"
-  )
+  .connect(process.env.MONGO_URI || "mongodb+srv://user1:user@cluster0.mmap8.mongodb.net/testDataDev")
   .then(() => console.log("Database is connected..."))
   .catch((err) => console.log(err));
 
-app.get("/dateEnable", fetchDatesEnable);
+
+app.get("/dateEnable" , 
+  (req, res) => {
+    res.send("Hello World!");
+  }
+);
 
 // production script
 app.use(express.static("./frontend/build"));
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
 });
+
 
 app.listen(port, () => {
   console.log(`Server is running on post ${port}`);
