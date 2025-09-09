3d55f0cef316a9e2cd100d779856fc05f5ffa881
Nalinda Deshapriya <nalindadkumarasinghe@gmail.com>
2025-09-09 13:37:01 +0530

    changed functions

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,175 +1,96 @@
-import React, { useState } from "react";
-import { NavLink, useLocation } from "react-router-dom";
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="container-fluid position-relative p-0">
-      <nav className="navbar navbar-expand-lg navbar-dark px-5 py-3 py-lg-0">
-        <a href="/" className="navbar-brand p-0">
-          <h1 className="m-0">
-            <i className="fa fa-user-tie me-2"></i>Startup
-          </h1>
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarCollapse"
-        >
-          <span className="fa fa-bars"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarCollapse">
-          <div className="navbar-nav ms-auto py-0">
-            <a href="/" className="nav-item nav-link active">
-              Home
-            </a>
-            <a href="/about" className="nav-item nav-link">
-              About
-            </a>
-            <a href="/service" className="nav-item nav-link">
-              Services
-            </a>
-            <div className="nav-item dropdown">
-              <a
-                href="#"
-                className="nav-link dropdown-toggle"
-                data-bs-toggle="dropdown"
-              >
-                Blog
-              </a>
-              <div className="dropdown-menu m-0">
-                <a href="/blog" className="dropdown-item">
-                  Blog Grid
-                </a>
-                <a href="/detail" className="dropdown-item">
-                  Blog Detail
-                </a>
-              </div>
-            </div>
-            <div className="nav-item dropdown">
-              <a
-                href="#"
-                className="nav-link dropdown-toggle"
-                data-bs-toggle="dropdown"
-              >
-                Pages
-              </a>
-              <div className="dropdown-menu m-0">
-                <a href="/price" className="dropdown-item">
-                  Pricing Plan
-                </a>
-                <a href="/feature" className="dropdown-item">
-                  Our features
-                </a>
-                <a href="/team" className="dropdown-item">
-                  Team Members
-                </a>
-                <a href="/testimonial" className="dropdown-item">
-                  Testimonial
-                </a>
-                <a href="/quote" className="dropdown-item">
-                  Free Quote
-                </a>
-              </div>
-            </div>
-            <a href="/contact" className="nav-item nav-link">
-              Contact
-            </a>
-          </div>
-          <button
-            type="button"
-            className="btn text-primary ms-3"
-            data-bs-toggle="modal"
-            data-bs-target="#searchModal"
-          >
-            <i className="fa fa-search"></i>
-          </button>
-        </div>
-      </nav>
-
-      <div
-        id="header-carousel"
-        className="carousel slide carousel-fade"
-        data-bs-ride="carousel"
+    <nav
+      className="navbar navbar-expand-lg navbar-dark px-5 py-3 py-lg-0"
+      style={{
+        position: "sticky",
+        top: 0,
+      }}
+    >
+      <NavLink to="/" className="navbar-brand p-0">
+        <h1 className="m-0">
+          <i className="fa fa-user-tie me-2"></i>Startup
+        </h1>
+      </NavLink>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarCollapse"
       >
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <img className="w-100" src="img/carousel-1.jpg" alt="Image" />
-            <div className="carousel-caption d-flex flex-column align-items-center justify-content-center">
-              <div className="p-3" style={{ maxWidth: 900 }}>
-                <h5 className="text-white text-uppercase mb-3 animated slideInDown">
-                  Creative & Innovative
-                </h5>
-                <h1 className="display-1 text-white mb-md-4 animated zoomIn">
-                  Creative & Innovative Digital Solution
-                </h1>
-                <a
-                  href="/quote"
-                  className="btn btn-primary py-md-3 px-md-5 me-3 animated slideInLeft"
-                >
-                  Free Quote
-                </a>
-                <a
-                  href="/contact"
-                  className="btn btn-outline-light py-md-3 px-md-5 animated slideInRight"
-                >
-                  Contact Us
-                </a>
-              </div>
+        <span className="fa fa-bars"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarCollapse">
+        <div className="navbar-nav ms-auto py-0">
+          <NavLink to="/" className="nav-item nav-link">
+            Home
+          </NavLink>
+          <NavLink to="/about" className="nav-item nav-link">
+            About
+          </NavLink>
+          <NavLink to="/service" className="nav-item nav-link">
+            Services
+          </NavLink>
+          <div className="nav-item dropdown">
+            <span
+              className="nav-link dropdown-toggle"
+              data-bs-toggle="dropdown"
+              style={{ cursor: "pointer" }}
+            >
+              Blog
+            </span>
+            <div className="dropdown-menu m-0">
+              <NavLink to="/blog" className="dropdown-item">
+                Blog Grid
+              </NavLink>
+              <NavLink to="/detail" className="dropdown-item">
+                Blog Detail
+              </NavLink>
             </div>
           </div>
-          <div className="carousel-item">
-            <img className="w-100" src="img/carousel-2.jpg" alt="Image" />
-            <div className="carousel-caption d-flex flex-column align-items-center justify-content-center">
-              <div className="p-3" style={{ maxWidth: 900 }}>
-                <h5 className="text-white text-uppercase mb-3 animated slideInDown">
-                  Creative & Innovative
-                </h5>
-                <h1 className="display-1 text-white mb-md-4 animated zoomIn">
-                  Creative & Innovative Digital Solution
-                </h1>
-                <a
-                  href="/quote"
-                  className="btn btn-primary py-md-3 px-md-5 me-3 animated slideInLeft"
-                >
-                  Free Quote
-                </a>
-                <a
-                  href="/contact"
-                  className="btn btn-outline-light py-md-3 px-md-5 animated slideInRight"
-                >
-                  Contact Us
-                </a>
-              </div>
+          <div className="nav-item dropdown">
+            <span
+              className="nav-link dropdown-toggle"
+              data-bs-toggle="dropdown"
+              style={{ cursor: "pointer" }}
+            >
+              Pages
+            </span>
+            <div className="dropdown-menu m-0">
+              <NavLink to="/price" className="dropdown-item">
+                Pricing Plan
+              </NavLink>
+              <NavLink to="/feature" className="dropdown-item">
+                Our features
+              </NavLink>
+              <NavLink to="/team" className="dropdown-item">
+                Team Members
+              </NavLink>
+              <NavLink to="/testimonial" className="dropdown-item">
+                Testimonial
+              </NavLink>
+              <NavLink to="/quote" className="dropdown-item">
+                Free Quote
+              </NavLink>
             </div>
           </div>
+          <NavLink to="/contact" className="nav-item nav-link">
+            Contact
+          </NavLink>
         </div>
         <button
-          className="carousel-control-prev"
           type="button"
-          data-bs-target="#header-carousel"
-          data-bs-slide="prev"
+          className="btn text-primary ms-3"
+          data-bs-toggle="modal"
+          data-bs-target="#searchModal"
         >
-          <span
-            className="carousel-control-prev-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button
-          className="carousel-control-next"
-          type="button"
-          data-bs-target="#header-carousel"
-          data-bs-slide="next"
-        >
-          <span
-            className="carousel-control-next-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Next</span>
+          <i className="fa fa-search"></i>
         </button>
       </div>
-    </div>
+    </nav>
   );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,96 @@ import React from "react";
 import HomepageBlog from "../components/HomepageBlog";
 import Team from "../components/Team";
 import Navbar from "../components/Navbar";
+import { NavLink } from "react-router-dom";
 
 const Home = () => {
   return (
     <>
+      <div className="container-fluid position-relative p-0">
+        
+        <div
+          id="header-carousel"
+          className="carousel slide carousel-fade"
+          data-bs-ride="carousel"
+        >
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img className="w-100" src="img/carousel-1.jpg" alt="Image" />
+              <div className="carousel-caption d-flex flex-column align-items-center justify-content-center">
+                <div className="p-3" style={{ maxWidth: 900 }}>
+                  <h5 className="text-white text-uppercase mb-3 animated slideInDown">
+                    Creative & Innovative
+                  </h5>
+                  <h1 className="display-1 text-white mb-md-4 animated zoomIn">
+                    Creative & Innovative Digital Solution
+                  </h1>
+                  <NavLink
+                    to="/quote"
+                    className="btn btn-primary py-md-3 px-md-5 me-3 animated slideInLeft"
+                  >
+                    Free Quote
+                  </NavLink>
+                  <NavLink
+                    to="/contact"
+                    className="btn btn-outline-light py-md-3 px-md-5 animated slideInRight"
+                  >
+                    Contact Us
+                  </NavLink>
+                </div>
+              </div>
+            </div>
+            <div className="carousel-item">
+              <img className="w-100" src="img/carousel-2.jpg" alt="Image" />
+              <div className="carousel-caption d-flex flex-column align-items-center justify-content-center">
+                <div className="p-3" style={{ maxWidth: 900 }}>
+                  <h5 className="text-white text-uppercase mb-3 animated slideInDown">
+                    Creative & Innovative
+                  </h5>
+                  <h1 className="display-1 text-white mb-md-4 animated zoomIn">
+                    Creative & Innovative Digital Solution
+                  </h1>
+                  <NavLink
+                    to="/quote"
+                    className="btn btn-primary py-md-3 px-md-5 me-3 animated slideInLeft"
+                  >
+                    Free Quote
+                  </NavLink>
+                  <NavLink
+                    to="/contact"
+                    className="btn btn-outline-light py-md-3 px-md-5 animated slideInRight"
+                  >
+                    Contact Us
+                  </NavLink>
+                </div>
+              </div>
+            </div>
+          </div>
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-bs-target="#header-carousel"
+            data-bs-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button
+            className="carousel-control-next"
+            type="button"
+            data-bs-target="#header-carousel"
+            data-bs-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
+      </div>
       {/* Full Screen Search Start */}
       <div className="modal fade" id="searchModal" tabIndex="-1">
         <div className="modal-dialog modal-fullscreen">
